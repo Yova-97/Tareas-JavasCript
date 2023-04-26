5f8aa3d00e38173932d82667fbcaa1bf33ec2e02
Yova <107813052+Yova-97@users.noreply.github.com>
2023-04-25 20:45:13 -0600

    Add files via upload

---
+++ Retos Sprint 2/Retos-Sprint2  notas.js
@@ -0,0 +1,13 @@
+let nota = 100;
+if (nota > 89){
+    console.log("Excelente");
+}
+else if (nota > 74 ){
+    console.log("Muy bien");
+}
+else if (nota > 64 ){
+    console.log("Bien");
+}
+else{
+    console.log("Reprobado")
+}
