035853c3e5c1ad2b7cc208b08e75ce4a4d4255ad
Yova <107813052+Yova-97@users.noreply.github.com>
2023-05-10 20:49:25 -0600

    Add files via upload

---
+++ Ejerc.4_Funciones/calculo_IVA.js
@@ -0,0 +1,17 @@
+//calcular IVA
+function CalcularIva(monto){
+    const iva = 0.12;
+    const ivaCalculado = monto * iva;
+    return ivaCalculado;
+  }
+  
+  const monto = 1000;
+  const ivaCalculado = CalcularIva(monto);
+  console.log(ivaCalculado); // Devuelve 120
+   //Nombre
+   function saludar(nombre) {
+    return `Hola ${nombre}, bienvenid@`;
+   }
+   const nombre = "Luis";
+   const mensaje = saludar(nombre);
+   console.log(mensaje);
