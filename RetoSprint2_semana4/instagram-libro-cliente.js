f5e359dbefab738453d80351d621be1947f935cd
Yova <107813052+Yova-97@users.noreply.github.com>
2023-05-15 00:01:13 -0600

    Add files via upload

---
+++ RetoSprint2_semana4/instagram-libro-cliente.js
@@ -0,0 +1,33 @@
+let cuentaInstagram = {
+    nombreDeUsuario:"Luis",
+    correo:"example67@gmail.com",
+    contraseña:123456
+};
+console.log("CUENTA DE INSTAGRAM");
+console.log("Nombre de Usuario: "+cuentaInstagram.nombreDeUsuario);
+console.log("Correo: "+cuentaInstagram.correo);
+console.log("Contraseña "+cuentaInstagram.contraseña);
+
+let libro = {
+    Titulo: "50 Sombras de Grey",
+    Autor: "E.L. James",
+    FechaDePublicacion: 2011,
+    PalabrasClave: "BDSM, literatura contemporánea"
+};
+console.log("LIBRO DE UNA BIBLIOTECA");
+console.log("Título: "+libro.Titulo);
+console.log("Autor: "+libro.Autor);
+console.log("Fecha de Publicaciòn: "+libro.FechaDePublicacion);
+console.log("Palabras clave: "+libro.PalabrasClave);
+
+let ClienteRestaurante = {
+    NombreCompleto: "Luis Vazqúez",
+    Telefono: 46497321,
+    CorreoElectronico:"example2@gmail.com",
+    MetodoDePago:"Efectivo O Tarjeta de Crédito"
+};
+console.log("CLIENTE DE UN RESTAURANTE");
+console.log("Nombre Completo: "+ClienteRestaurante.NombreCompleto);
+console.log("Teléfono: "+ClienteRestaurante.Telefono);
+console.log("Correo Electronico: "+ClienteRestaurante.CorreoElectronico);
+console.log("Método de Pago: "+ClienteRestaurante.MetodoDePago);
